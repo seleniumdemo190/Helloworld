1f16647ec7e94f9550d4726df0da0d47edd3aced
shanu <shanu@shanu-PC>
2018-10-01 13:56:10 +0530

    fffj

--- src/first/vbb.java
+++ src/first/vbb.java
@@ -2,8 +2,10 @@ package first;
 
 public class vbb {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		// TODO Auto-generated method stub
+		System.out.println("sadsd");
 
 	}
 
